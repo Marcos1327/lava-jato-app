7832bdbe6134c0800ed161d9a15ed88a21986de4
Marcos Giovanny <giovannygigi@gmail.com>
2025-08-18 22:36:26 -0300

    organized imports

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
-import Home from './pages/Home/Home';
-import VeiculosList from '../src/pages/Veiculo/VeiculosList/VeiculosList';
-import ProdutosList from '../src/pages/Produto/ProdutosList/ProdutoList';
-import ServicosList from '../src/pages/TipoServico/ServicosList/ServicosList';
-import FuncionarioList from '../src/pages/Equipe/FuncionarioList/FuncionarioList';
 import GlobalStyles from './styles/globalStyles'
 import AppRoutes from './routes/AppRoutes';
 
