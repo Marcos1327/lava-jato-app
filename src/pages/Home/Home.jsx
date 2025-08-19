ac7dbfe37e6f40306e2f7c3926e01fc6395f95fc
Marcos Giovanny <giovannygigi@gmail.com>
2025-08-18 23:01:30 -0300

    ajusto

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,18 +1,18 @@
-import React, { use } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Title, Button, ButtonGroup, Container } from './styles';
 
 function Home() {
-    const Navigate = useNavigate();
+    const navigate = useNavigate();
     return (
         <Container >
             <Title>Lava a Jato</Title>
             <ButtonGroup>
-                <Button onClick={() => Navigate('clientes')}>Clientes</Button>
-                <Button onClick={() => Navigate('produtos')}>Produtos</Button>
-                <Button onClick={() => Navigate('veiculos')}>Veiculos</Button>
-                <Button onClick={() => Navigate('servicos')}>Servicos</Button>
-                <Button onClick={() => Navigate('funcionarios')}>Funcionarios</Button>
+                <Button onClick={() => navigate('clientes')}>Clientes</Button>
+                <Button onClick={() => navigate('produtos')}>Produtos</Button>
+                <Button onClick={() => navigate('veiculos')}>Veiculos</Button>
+                <Button onClick={() => navigate('servicos')}>Servicos</Button>
+                <Button onClick={() => navigate('funcionarios')}>Funcionarios</Button>
             </ButtonGroup>
         </Container>
     )
