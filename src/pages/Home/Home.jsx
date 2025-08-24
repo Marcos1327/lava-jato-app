cce2ee466a439be68e11845f196e8ed7b860a2c2
Marcos Giovanny <giovannygigi@gmail.com>
2025-08-24 11:42:44 -0300

    remove style component dependency

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,20 +5,9 @@ import { Title, Button, ButtonGroup, Container, Logo } from './styles';
 function Home() {
     const navigate = useNavigate();
     return (
-        <Container >
-            <div className = "sidebar">
-                <Logo>
-                    <Title>Lava a Jato</Title>
-                </Logo>
-                <ButtonGroup>
-                    <Button onClick={() => navigate('clientes')}>Clientes</Button>
-                    <Button onClick={() => navigate('produtos')}>Produtos</Button>
-                    <Button onClick={() => navigate('veiculos')}>Veiculos</Button>
-                    <Button onClick={() => navigate('servicos')}>Servicos</Button>
-                    <Button onClick={() => navigate('funcionarios')}>Funcionarios</Button>
-                </ButtonGroup>
-            </div>
-        </Container>
+        <div className='container'>
+            
+        </div>
     )
 }
 
