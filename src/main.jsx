9575cdde3cb48d4315d98618b2643d1dbc675291
Marcos Giovanny <giovannygigi@gmail.com>
2025-08-25 09:15:14 -0300

    configuração BASENAME browserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,11 @@ import './styles/global.css'
 import './assets/fonts/Poppins/fonts.css'
 import AppRoutes from './routes/AppRoutes';
 
+const basename = import.meta.env.BASE_URL
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <AppRoutes />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/lava-jato-app',
+  define: {
+    __BASENAME__: '"/lava-jato-app"',
+  },
 })
